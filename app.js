6578264539d843e7231850f9d972fe9e38d2fa80
Kapza <lukakapetanovic@icloud.com>
2023-11-10 18:53:22 +0100

    added innerHTML and function structure. Still IP

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-//FML
+// X years = 2023 (current year) - 1994(birth year) = 29 years
+// Y months = 11 (current month) - 6(birth month) = 5 months- minus 1 month so i have to carry over the days.
+// Z days = 8 (current day) - 15 (birth day) = -7 then i have to plus 31 so then its 24
 
-//Age calculator
-// get todays date EXACT 
-// 3 input fields- Day- Month- Year.
 
-//getDate()
-//setDate()
-
+//Current Date
 const date = new Date();
 
 let currentDay = String (date.getDate()).padStart(2, '0');//The padStart is what adds a '0' before each single int. So its is 01,02,03 etc
@@ -15,31 +12,41 @@ let currentYear = String(date.getFullYear());
 
 let today = currentDay+ '/'+currentMonth+ '/'+currentYear; // gets todays date
 
+//User Input fields
+const inputDay = document.getElementById('input-date-day');
+const inputMonth = document.getElementById('input-date-month');
+const inputYear = document.getElementById('input-date-year');
 
 
-
-// X years = 2023 (current year) - 1994(birth year) = 29 years
-// Y months = 11 (current month) - 6(birth month) = 5 months- minus 1 month so i have to carry over to days.
-// Z days = 8 (current day) - 15 (birth day) = -7 then i have to plus 31 so then its 24
-
-const inputDateDay = document.getElementById('input-date-day');
-const inputDateMonth = document.getElementById('input-date-month');
-const inputDateYear = document.getElementById('input-date-year');
 
 const submitButton = document.getElementById('submit-btn');
 
-inputDateDay.addEventListener('input', (event) => {
+//Input Values Output
+const outputDay = document.getElementById('input-display-day');
+// const outputDayValue = outputDay.value;
+const outputMonth = document.getElementById('input-display-month');
+const outputYear =document.getElementById('input-display-year');
+
+//submit formula
+submitButton.addEventListener('click', () =>{
+    ////User's typed Values
+let inputDayValue = inputDay.value;
+let inputMonthValue = inputMonth.value;
+let inputYearValue = inputYear.value;
+    //Year result calculation
+    const yearResult = currentYear - inputYearValue;
+    outputDay.innerHTML = yearResult;
+
+    //Month result calculation
+    const monthResult = currentMonth - inputMonthValue;
+    outputMonth.innerHTML = monthResult;
+
+    //Day result calculation
+    const dayResult = currentDay - inputDayValue;
+    outputYear.innerHTML = dayResult;
+    
+    // if(){
+
+    // }
 
 })
-
-
-inputDateMonth.addEventListener('input', (event) => {
-
-})
-
-
-inputDateYear.addEventListener('input', (event) => {
-
-})
-
-
